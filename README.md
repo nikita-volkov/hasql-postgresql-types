b28fe057f6f79e53bd63119409a907cc0beba853
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-01-22 23:06:28 +0300

    Polish the docs

--- README.md
+++ README.md
@@ -3,5 +3,22 @@
 [![Hackage](https://img.shields.io/hackage/v/hasql-postgresql-types.svg)](https://hackage.haskell.org/package/hasql-postgresql-types)
 [![Continuous Haddock](https://img.shields.io/badge/haddock-master-blue)](https://nikita-volkov.github.io/hasql-postgresql-types/)
 
-Integration of "hasql" with ["postgresql-types"](https://github.com/nikita-volkov/postgresql-types).
-Provides codecs for all types having instance of `IsScalar` from ["postgresql-types-algebra"](https://github.com/nikita-volkov/postgresql-types-algebra).
+Integration of ["hasql"](https://github.com/nikita-volkov/hasql) with ["postgresql-types"](https://github.com/nikita-volkov/postgresql-types) via the `IsScalar` typeclass from ["postgresql-types-algebra"](https://github.com/nikita-volkov/postgresql-types-algebra).
+Provides automatic encoder and decoder generation for precise PostgreSQL scalar types.
+
+## Usage
+
+```haskell
+import Hasql.PostgresqlTypes (encoder, decoder)
+import qualified PostgresqlTypes as Pt
+import qualified Hasql.Statement as Statement
+import qualified Hasql.Encoders as Encoders
+import qualified Hasql.Decoders as Decoders
+
+myStatement :: Statement.Statement Pt.Timestamptz [Pt.Timestamptz]
+myStatement = Statement.preparable sql enc dec
+  where
+    sql = "SELECT $1::timestamptz"
+    enc = Encoders.param (Encoders.nonNullable encoder)
+    dec = Decoders.rowList (Decoders.column (Decoders.nonNullable decoder))
+```
--- src/library/Hasql/PostgresqlTypes.hs
+++ src/library/Hasql/PostgresqlTypes.hs
@@ -1,6 +1,24 @@
 -- |
--- This module provides a bridge between PostgreSQL's standard types and the Hasql library,
+-- This module provides a bridge between the types defined in the ["postgresql-types"](https://hackage.haskell.org/package/postgresql-types) and the ["hasql"](https://hackage.haskell.org/package/hasql) library,
 -- offering automatic encoder and decoder generation for types that implement the 'IsScalar' constraint.
+--
+-- == Usage Example
+--
+-- > import Hasql.PostgresqlTypes (encoder, decoder)
+-- > import qualified PostgresqlTypes as Pt
+-- > import qualified Hasql.Statement as Statement
+-- > import qualified Hasql.Encoders as Encoders
+-- > import qualified Hasql.Decoders as Decoders
+-- >
+-- > myStatement :: Statement.Statement Pt.Timestamptz [Pt.Timestamptz]
+-- > myStatement = Statement.preparable sql enc dec
+-- >   where
+-- >     sql = "SELECT $1::timestamptz"
+-- >     enc = Encoders.param (Encoders.nonNullable encoder)
+-- >     dec = Decoders.rowList (Decoders.column (Decoders.nonNullable decoder))
+--
+-- The 'encoder' and 'decoder' functions work with any type having an 'IsScalar' instance,
+-- automatically handling binary encoding/decoding and OID resolution.
 module Hasql.PostgresqlTypes
   ( encoder,
     decoder,
@@ -18,6 +36,9 @@ import qualified TextBuilder
 import Prelude
 
 -- | Hasql value encoder for a PostgreSQL standard type.
+--
+-- Generates a Hasql value encoder for any type implementing 'IsScalar'.
+-- The encoder handles type resolution, and binary encoding automatically.
 encoder :: forall a. (IsScalar a) => Encoders.Value a
 encoder =
   Encoders.custom
@@ -29,6 +50,9 @@ encoder =
     (TextBuilder.toText . textualEncoder)
 
 -- | Hasql value decoder for a PostgreSQL standard type.
+--
+-- Generates a Hasql value decoder for any type implementing 'IsScalar'.
+-- The decoder handles type resolution, and binary decoding with proper error handling.
 decoder :: forall a. (IsScalar a) => Decoders.Value a
 decoder =
   Decoders.custom
