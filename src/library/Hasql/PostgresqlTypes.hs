b8cb8fe1e7ebf76dc03ecdc78990939aca09d429
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-01-26 14:48:56 +0300

    Try avoiding the class reexport with preservation of docs

--- src/library/Hasql/PostgresqlTypes.hs
+++ src/library/Hasql/PostgresqlTypes.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 -- |
 -- This module provides a bridge between the types defined in the ["postgresql-types"](https://hackage.haskell.org/package/postgresql-types) and the ["hasql"](https://hackage.haskell.org/package/hasql) library,
 -- offering automatic encoder and decoder generation for types that implement the 'IsScalar' constraint.
@@ -20,10 +22,166 @@
 --
 -- The 'encoder' and 'decoder' functions work with any type having an 'IsScalar' instance,
 -- automatically handling binary encoding/decoding and OID resolution.
-module Hasql.PostgresqlTypes
-  ( Hasql.Mapping.IsScalar,
-  )
-where
+module Hasql.PostgresqlTypes () where
 
+import GHC.TypeLits (KnownNat)
 import qualified Hasql.Mapping
-import Hasql.PostgresqlTypes.Mapping ()
+import qualified Hasql.PostgresqlTypes.Core as Core
+import PostgresqlTypes
+import PostgresqlTypes.Algebra
+
+instance (KnownNat len) => Hasql.Mapping.IsScalar (Bit len) where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Bool where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Box where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance (KnownNat len) => Hasql.Mapping.IsScalar (Bpchar len) where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Bytea where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Char where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Cidr where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Circle where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Date where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Float4 where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Float8 where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Hstore where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Inet where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Int2 where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Int4 where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Int8 where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Interval where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Json where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Jsonb where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Line where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Lseg where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Macaddr where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Macaddr8 where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Money where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance (IsMultirangeElement element) => Hasql.Mapping.IsScalar (Multirange element) where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance (KnownNat precision, KnownNat scale) => Hasql.Mapping.IsScalar (Numeric precision scale) where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Oid where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Path where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Point where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Polygon where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance (IsRangeElement element) => Hasql.Mapping.IsScalar (Range element) where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Text where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Time where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Timestamp where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Timestamptz where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Timetz where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance Hasql.Mapping.IsScalar Uuid where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance (KnownNat maxLen) => Hasql.Mapping.IsScalar (Varbit maxLen) where
+  encoder = Core.encoder
+  decoder = Core.decoder
+
+instance (KnownNat maxLen) => Hasql.Mapping.IsScalar (Varchar maxLen) where
+  encoder = Core.encoder
+  decoder = Core.decoder
